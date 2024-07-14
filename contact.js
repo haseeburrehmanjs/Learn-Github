743389bc1f25a0a3d045756481233f12debc67a3
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-07-14 10:35:40 +0500

    user object created

---
+++ contact.js
@@ -0,0 +1,13 @@
+let userData = {
+    username : 'haseeb ur rehman',
+    age : 17,
+    emailId : 'haseeburrehmanjs@gmail.com',
+    hobbies : [
+        'swimming',
+        'eating', 
+        'cricket',
+        'football'
+    ]
+}
+
+console.log(userData['emailId']);
