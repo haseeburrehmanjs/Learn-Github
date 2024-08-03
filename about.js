5d85c581f73e418b1c54f908d39117c79a39517d
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-03 20:19:56 +0500

    some changes in about

--- about.js
+++ about.js
@@ -2,3 +2,10 @@ let about = 'haseeb ur rehman js'
 console.log(about);
 
 let userName = 'Haseeb ur Rehman js'
+
+
+let mySelf = {
+    name : 'haseeb ur rehman',
+    age: 17,
+    fatherName: 'kamran ahmed'
+}
